55e0d22299c7fb6d684d162269da48dd1f40bba6
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-06-22 17:33:28 +0500

    set and get data from localStaroage done

--- js/script.js
+++ js/script.js
@@ -3,55 +3,76 @@ const todo_input = document.getElementById('todo-input');
 const todos_container = document.getElementById('todos-container');
 const warning_pupup = document.getElementById('warning-popup');
 
-let ele = document.querySelector("sl");
+// get data from local storage 
+let local_Storage_Data = JSON.parse(localStorage.getItem("todosData"));
+console.log(local_Storage_Data);
+
+// adding all todos to todo container 
+local_Storage_Data.forEach(todo => Create_Todo_Item(todo));
 
 
-let local_Storage_Data = [
-    { "id": 1, "todo": "Complete TodoList project", "completed": false },
-    { "id": 2, "todo": "Write unit tests for new features", "completed": false },
-    { "id": 3, "todo": "Update project documentation", "completed": true },
-    { "id": 4, "todo": "Fix bugs reported by QA team", "completed": false },
-    { "id": 10, "todo": "kanwal", "completed": true },
-    { "id": 5, "todo": "Review pull requests from team", "completed": true },
-    { "id": 6, "todo": "Plan sprint tasks for next week", "completed": false },
-    { "id": 7, "todo": "Optimize database queries", "completed": false },
-    { "id": 8, "todo": "Refactor legacy code modules", "completed": true },
-    { "id": 9, "todo": "Prepare presentation for client meeting", "completed": false },
-    { "id": 10, "todo": "Conduct code review session", "completed": true },
-]
-
-
-
-
+// get value from input for new todo 
 todo_form.addEventListener('submit', e => {
     e.preventDefault();
 
 
     let todo_input_data = todo_input.value.trim();
-
     let duplicate_entry = local_Storage_Data.some(element => element.todo === todo_input_data);
-    duplicate_entry && warning_pupup.toast();
 
-
-
-
-
-    // check duplicate data 
-    // Trim white spaces 
-    // sanitize data
-    // call function to create and add into todolist
+    // show pop up on duplicate enty 
+    duplicate_entry ? warning_pupup.toast() : Create_Todo_Item({ "todo": todo_input_data });
 
     todo_input.value = "";
+
+    // check duplicate data   done
+    // Trim white spaces      done 
+    // sanitize data   
+    // call function to create and add into todolist
 })
 
 
 
-// get data from local storage
-
-// get data from input
 
 // create todo
 
+function Create_Todo_Item({ "id": todo_id, "todo": todo_name, "completed": todo_completed }) {
+
+    const todo_item = document.createElement('div');
+    todo_item.className = "todo-item";
+
+    const todo_data = document.createElement('div');
+    todo_data.className = "data";
+
+    const todo_checkbox = document.createElement('input');
+    todo_checkbox.type = "checkbox";
+
+    let todo_input = document.createElement('input');
+    todo_input.value = todo_name;
+    todo_input.type = "text";
+    todo_input.readOnly = true;
+
+    let todo_actions = document.createElement('div');
+    todo_actions.className = "actions";
+
+    let todo_delete_btn = document.createElement('button');
+    todo_delete_btn.className = "delete";
+    todo_delete_btn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+
+    let todo_edit_btn = document.createElement('button');
+    todo_edit_btn.className = "edit";
+    todo_edit_btn.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+
+    todo_data.appendChild(todo_checkbox);
+    todo_data.appendChild(todo_input);
+    todo_actions.appendChild(todo_edit_btn);
+    todo_actions.appendChild(todo_delete_btn);
+
+    todo_item.appendChild(todo_data);
+    todo_item.appendChild(todo_actions);
+    todos_container.appendChild(todo_item);
+}
+
+// Create_Todo_Item();
 // update todo
 
 // delete todo 
