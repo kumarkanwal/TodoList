dc7845a85eaafc427694802641c41cb8b61de1e2
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-06-25 14:13:01 +0500

    let commit js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ const todos_container = document.getElementById('todos-container');
 const warning_pupup = document.getElementById('warning-popup');
 
 // get data from local storage 
-let local_Storage_Data = JSON.parse(localStorage.getItem("todosData"));
-console.log(local_Storage_Data);
+function get_Data_LocalStorage() {
+    return JSON.parse(localStorage.getItem("todosData"));
+}
+
+let local_Storage_Data = get_Data_LocalStorage();
+console.log(local_Storage_Data)
 
 // adding all todos to todo container 
-local_Storage_Data.forEach(todo => Create_Todo_Item(todo));
+local_Storage_Data.todos.forEach(todo => Create_Todo_Item(todo));
 
 
 // get value from input for new todo 
@@ -17,10 +21,29 @@ todo_form.addEventListener('submit', e => {
 
 
     let todo_input_data = todo_input.value.trim();
-    let duplicate_entry = local_Storage_Data.some(element => element.todo === todo_input_data);
+    let duplicate_entry = local_Storage_Data.todos.some(element => element.todo === todo_input_data);
+
 
     // show pop up on duplicate enty 
-    duplicate_entry ? warning_pupup.toast() : Create_Todo_Item({ "todo": todo_input_data });
+    if (duplicate_entry) {
+        warning_pupup.toast()
+
+    } else {
+        Create_Todo_Item({ "todo": todo_input_data });
+
+        local_Storage_Data = get_Data_LocalStorage();
+        let new_todo_id = ++local_Storage_Data.id;
+
+        const new_todo_data = { "id": new_todo_id, "todo": todo_input_data, "completed": false };
+        local_Storage_Data.todos.push(new_todo_data);
+        local_Storage_Data.id = new_todo_id;
+
+        console.log(local_Storage_Data);
+
+
+        // local_Storage_Data.
+
+    }
 
     todo_input.value = "";
 
